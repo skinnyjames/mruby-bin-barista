skinnyjames/mruby-bin-mrbuild

feat(initial): initial commit

lib/commands/block.rb
@@ -1,0 +1,15 @@
+module MRBuild
+  module Commands
+    class Block < Base
+      attr_reader :block
+    
+      def initialize(&block)
+        @block = block
+      end
+
+      def execute
+        block.call
+      end
+    end
+  end
+end

lib/graph.rb
@@ -1,0 +1,97 @@
+module MRBuild
+  class GraphCyclicalError < StandardError; end
+
+  class Graph
+    attr_reader :nodes, :vertices
+
+    def initialize(nodes = [], vertices = {})
+      @nodes = nodes
+      @vertices = vertices
+    end
+
+    def add(node)
+      if vertex = vertices[node]
+        return vertex
+      end
+
+      vertex = Vertex.new(name: node)
+      vertices[node] = vertex
+      nodes << node
+
+      vertex
+    end
+
+    def add_edge(from, to)
+      return if from == to
+
+      from_vertex = add(from)
+      to_vertex = add(to)
+
+      return if to_vertex.incoming[from]
+
+      Graph::Visitor.visit(vertex: from_vertex, callback: ensure_non_cylical(to))
+
+      from_vertex.has_outgoing = true
+
+      to_vertex.incoming[from] = from_vertex
+      to_vertex.incoming_names << from
+
+      vertices[from] = from_vertex
+      vertices[to] = to_vertex
+    end
+
+    def filter(names, result = names.dup)
+      return result.uniq if names.empty?
+
+      name = names.shift
+      vertex = vertices[name]
+      result = (result + vertex.incoming_names)
+
+      result.concat filter((vertex.incoming_names - names), result)
+      filter(names, result)
+    end
+
+    def ensure_non_cylical(to)
+      ->(vertex, path) do
+        if vertex.name == to
+          raise GraphCyclicalError.new("Cyclical reference detected: #{to} <- #{path.join(" <- ")}")
+        end
+      end
+    end
+  end
+
+  class Graph::Vertex
+    attr_accessor :name, :incoming, :incoming_names, :has_outgoing
+
+    def initialize(name:, incoming: {}, incoming_names: [], has_outgoing: false)
+      @name = name
+      @incoming = incoming
+      @incoming_names = incoming_names
+      @has_outgoing = has_outgoing
+    end
+
+    def has_outgoing?
+      has_outgoing
+    end
+  end
+
+  class Graph::Visitor
+    def self.visit(vertex:, callback:, visited: {}, path: [])
+      node = vertex.name
+      incoming = vertex.incoming
+      incoming_names = vertex.incoming_names
+
+      return if visited[node]
+
+      path << node
+      visited[node] = true
+
+      incoming_names.each do |name|
+        visit(vertex: incoming[name], callback: callback, visited: visited, path: path)
+      end
+
+      callback.call(vertex, path)
+      path.pop
+    end
+  end
+end

lib/resolvers/github.rb
@@ -1,0 +1,22 @@
+module MRBuild
+  class GithubResolver
+    attr_reader :repo, :branch
+
+    def initialize(**args)
+      @repo = args[:github]
+      @branch = args[:branch]
+    end
+
+    def location
+      "https://github.com/#{repo}"
+    end
+
+    def id
+      repo
+    end
+
+    def resolve!
+      IO.popen("git clone #{location}")
+    end
+  end
+end

lib/resolvers/no.rb
@@ -1,0 +1,11 @@
+module MRBuild
+  class NoResolver
+    def location
+      Dir.pwd
+    end
+
+    def resolve!
+
+    end
+  end
+end

setup.rb
@@ -1,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# thanks: https://github.com/iij/mruby-dir/blob/master/run_test.rb
+gemname = File.basename(File.dirname(File.expand_path __FILE__))
+
+if __FILE__ == $0
+  repository, dir = 'https://github.com/mruby/mruby.git', 'tmp/mruby'
+  build_args = ARGV
+  build_args = ['clean', 'all']  if build_args.nil? or build_args.empty?
+
+  Dir.mkdir 'tmp' unless File.exist?('tmp')
+  unless File.exist?(dir)
+    system "git clone #{repository} #{dir}"
+  end
+
+  system(%Q[cd #{dir}; MRUBY_CONFIG=#{__dir__}/build_config.rb rake #{build_args.join(' ')}])
+  exit system(%Q[cd #{__dir__}; #{dir}/bin/theorize --require="mrblib/mrbuild.rb" test])
+end

test/graph.rb
@@ -1,0 +1,85 @@
+module MRBuild
+  class GraphSpec
+    include Theorem::Hypothesis
+  
+    test "can add a node" do
+      with_graph(nodes: ["foo", "bar"]) do |graph|
+        expect(graph.nodes).to eql(["foo", "bar"])
+      end
+    end
+
+    test "returns a vertex with no edges" do
+      with_graph do |graph|
+        v1 = graph.add("foo")
+        expect(v1.class).to be(::MRBuild::Graph::Vertex)
+        expect(v1.incoming_names).to eql([])
+      end
+    end
+
+    test "add_edge connects two nodes" do
+      with_graph(nodes: ["foo", "bar"]) do |graph|
+        graph.add_edge("foo", "bar")
+    
+        foo = graph.vertices["foo"]
+        expect(foo.incoming_names).to eql([])
+        expect(foo.has_outgoing?).to be(true)
+
+        bar = graph.vertices["bar"]
+        expect(bar.incoming_names).to eql(["foo"])
+        expect(bar.has_outgoing?).to be(false)
+      end
+    end
+
+    test "can connect a node hierarchy" do
+      with_graph(
+        nodes: ["foo", "bar", "buzz"],
+        edges: [%w[foo bar], %w[foo buzz], %w[bar buzz]]
+      ) do |graph|
+        buzz = graph.vertices["buzz"]
+        
+        expect(buzz.incoming_names).to eql(["foo", "bar"])
+        expect(buzz.has_outgoing?).to be(false)
+
+        bar = graph.vertices["bar"]
+        expect(bar.incoming_names).to eql(["foo"])
+        expect(bar.has_outgoing?).to be(true)
+
+        foo = graph.vertices["foo"]
+        expect(foo.incoming_names).to eql([])
+        expect(foo.has_outgoing?).to be(true)
+      end
+    end
+
+    test "returns a list of tasks that match the filtered tree" do
+      with_graph(nodes: ["a", "b", "c", "d", "e"], edges: [%w[a e], %w[c e]]) do |graph|
+        expect(graph.filter(["e", "b"])).to eql(["e", "b", "a", "c"])
+      end
+    end
+
+    test "filters with a nested hierarchy" do
+      with_graph(nodes: ["a", "b", "c", "d", "e", "f", "g", "h"], edges: [%w[a c], %w[c d], %w[c f], %w[c h], %w[h e]]) do |graph|
+        expect(graph.filter(["e"])).to eql(["e", "h", "c", "a"])
+      end
+    end
+
+    # test "raises an exception on cyclical reference" do
+    #   with_graph(nodes: ["foo", "bar", "baz", "buzz"], edges: [{ "foo", "bar" }, { "bar", "baz" }, { "buzz", "foo" }]) do |graph|
+    #     expect -> { graph.add_edge("baz", "buzz") }.to raise_error
+    #   end
+    # end
+
+    def with_graph(nodes: [], edges: [], &block)
+      graph = ::MRBuild::Graph.new
+
+      nodes.each do |node|
+        graph.add(node)
+      end
+
+      edges.each do |tuple|
+        graph.add_edge(tuple[0], tuple[1])
+      end
+
+      block.call graph
+    end
+  end
+end

test/resolver.rb
@@ -1,0 +1,31 @@
+class ResolverSpec
+  include Theorem::Hypothesis
+
+  test "resolves git" do
+    resolver = resolve(git: "https://something.git")
+
+    expect(resolver.class).to be(::MRBuild::GitResolver)
+  end
+
+  test "resolves github" do
+    resolver = resolve(github: "skinnyjames/theorem")
+
+    expect(resolver.class).to be(::MRBuild::GithubResolver)
+  end
+
+  test "resolves http" do
+    resolver = resolve(http: "http://skinnyjames.net/something.tar.gz")
+
+    expect(resolver.class).to be(::MRBuild::HTTPResolver)
+  end
+
+  test "resolves local" do
+    resolver = resolve(path: "../some/dep")
+
+    expect(resolver.class).to be(::MRBuild::LocalResolver)
+  end
+
+  def resolve(**args)
+    ::MRBuild::Resolver.locate(**args)
+  end
+end

skinnyjames/mruby-bin-barista

lib/commands/block.rb
@@ -1,0 +1,15 @@
+module Barista
+  module Commands
+    class Block < Base
+      attr_reader :block
+    
+      def initialize(&block)
+        @block = block
+      end
+
+      def execute
+        block.call
+      end
+    end
+  end
+end

lib/graph.rb
@@ -1,0 +1,97 @@
+module Barista
+  class GraphCyclicalError < StandardError; end
+
+  class Graph
+    attr_reader :nodes, :vertices
+
+    def initialize(nodes = [], vertices = {})
+      @nodes = nodes
+      @vertices = vertices
+    end
+
+    def add(node)
+      if vertex = vertices[node]
+        return vertex
+      end
+
+      vertex = Vertex.new(name: node)
+      vertices[node] = vertex
+      nodes << node
+
+      vertex
+    end
+
+    def add_edge(from, to)
+      return if from == to
+
+      from_vertex = add(from)
+      to_vertex = add(to)
+
+      return if to_vertex.incoming[from]
+
+      Graph::Visitor.visit(vertex: from_vertex, callback: ensure_non_cylical(to))
+
+      from_vertex.has_outgoing = true
+
+      to_vertex.incoming[from] = from_vertex
+      to_vertex.incoming_names << from
+
+      vertices[from] = from_vertex
+      vertices[to] = to_vertex
+    end
+
+    def filter(names, result = names.dup)
+      return result.uniq if names.empty?
+
+      name = names.shift
+      vertex = vertices[name]
+      result = (result + vertex.incoming_names)
+
+      result.concat filter((vertex.incoming_names - names), result)
+      filter(names, result)
+    end
+
+    def ensure_non_cylical(to)
+      ->(vertex, path) do
+        if vertex.name == to
+          raise GraphCyclicalError.new("Cyclical reference detected: #{to} <- #{path.join(" <- ")}")
+        end
+      end
+    end
+  end
+
+  class Graph::Vertex
+    attr_accessor :name, :incoming, :incoming_names, :has_outgoing
+
+    def initialize(name:, incoming: {}, incoming_names: [], has_outgoing: false)
+      @name = name
+      @incoming = incoming
+      @incoming_names = incoming_names
+      @has_outgoing = has_outgoing
+    end
+
+    def has_outgoing?
+      has_outgoing
+    end
+  end
+
+  class Graph::Visitor
+    def self.visit(vertex:, callback:, visited: {}, path: [])
+      node = vertex.name
+      incoming = vertex.incoming
+      incoming_names = vertex.incoming_names
+
+      return if visited[node]
+
+      path << node
+      visited[node] = true
+
+      incoming_names.each do |name|
+        visit(vertex: incoming[name], callback: callback, visited: visited, path: path)
+      end
+
+      callback.call(vertex, path)
+      path.pop
+    end
+  end
+end

lib/resolvers/github.rb
@@ -1,0 +1,22 @@
+module Barista
+  class GithubResolver
+    attr_reader :repo, :branch
+
+    def initialize(**args)
+      @repo = args[:github]
+      @branch = args[:branch]
+    end
+
+    def location
+      "https://github.com/#{repo}"
+    end
+
+    def id
+      repo
+    end
+
+    def resolve!
+      IO.popen("git clone #{location}")
+    end
+  end
+end

lib/resolvers/no.rb
@@ -1,0 +1,11 @@
+module Barista
+  class NoResolver
+    def location
+      Dir.pwd
+    end
+
+    def resolve!
+
+    end
+  end
+end

setup.rb
@@ -1,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# thanks: https://github.com/iij/mruby-dir/blob/master/run_test.rb
+gemname = File.basename(File.dirname(File.expand_path __FILE__))
+
+if __FILE__ == $0
+  repository, dir = 'https://github.com/mruby/mruby.git', 'tmp/mruby'
+  build_args = ARGV
+  build_args = ['clean', 'all']  if build_args.nil? or build_args.empty?
+
+  Dir.mkdir 'tmp' unless File.exist?('tmp')
+  unless File.exist?(dir)
+    system "git clone #{repository} #{dir}"
+  end
+
+  system(%Q[cd #{dir}; MRUBY_CONFIG=#{__dir__}/build_config.rb rake #{build_args.join(' ')}])
+  exit system(%Q[cd #{__dir__}; #{dir}/bin/theorize --require="mrblib/barista.rb" test])
+end

test/graph.rb
@@ -1,0 +1,85 @@
+module Barista
+  class GraphSpec
+    include Theorem::Hypothesis
+  
+    test "can add a node" do
+      with_graph(nodes: ["foo", "bar"]) do |graph|
+        expect(graph.nodes).to eql(["foo", "bar"])
+      end
+    end
+
+    test "returns a vertex with no edges" do
+      with_graph do |graph|
+        v1 = graph.add("foo")
+        expect(v1.class).to be(::Barista::Graph::Vertex)
+        expect(v1.incoming_names).to eql([])
+      end
+    end
+
+    test "add_edge connects two nodes" do
+      with_graph(nodes: ["foo", "bar"]) do |graph|
+        graph.add_edge("foo", "bar")
+    
+        foo = graph.vertices["foo"]
+        expect(foo.incoming_names).to eql([])
+        expect(foo.has_outgoing?).to be(true)
+
+        bar = graph.vertices["bar"]
+        expect(bar.incoming_names).to eql(["foo"])
+        expect(bar.has_outgoing?).to be(false)
+      end
+    end
+
+    test "can connect a node hierarchy" do
+      with_graph(
+        nodes: ["foo", "bar", "buzz"],
+        edges: [%w[foo bar], %w[foo buzz], %w[bar buzz]]
+      ) do |graph|
+        buzz = graph.vertices["buzz"]
+        
+        expect(buzz.incoming_names).to eql(["foo", "bar"])
+        expect(buzz.has_outgoing?).to be(false)
+
+        bar = graph.vertices["bar"]
+        expect(bar.incoming_names).to eql(["foo"])
+        expect(bar.has_outgoing?).to be(true)
+
+        foo = graph.vertices["foo"]
+        expect(foo.incoming_names).to eql([])
+        expect(foo.has_outgoing?).to be(true)
+      end
+    end
+
+    test "returns a list of tasks that match the filtered tree" do
+      with_graph(nodes: ["a", "b", "c", "d", "e"], edges: [%w[a e], %w[c e]]) do |graph|
+        expect(graph.filter(["e", "b"])).to eql(["e", "b", "a", "c"])
+      end
+    end
+
+    test "filters with a nested hierarchy" do
+      with_graph(nodes: ["a", "b", "c", "d", "e", "f", "g", "h"], edges: [%w[a c], %w[c d], %w[c f], %w[c h], %w[h e]]) do |graph|
+        expect(graph.filter(["e"])).to eql(["e", "h", "c", "a"])
+      end
+    end
+
+    # test "raises an exception on cyclical reference" do
+    #   with_graph(nodes: ["foo", "bar", "baz", "buzz"], edges: [{ "foo", "bar" }, { "bar", "baz" }, { "buzz", "foo" }]) do |graph|
+    #     expect -> { graph.add_edge("baz", "buzz") }.to raise_error
+    #   end
+    # end
+
+    def with_graph(nodes: [], edges: [], &block)
+      graph = ::Barista::Graph.new
+
+      nodes.each do |node|
+        graph.add(node)
+      end
+
+      edges.each do |tuple|
+        graph.add_edge(tuple[0], tuple[1])
+      end
+
+      block.call graph
+    end
+  end
+end

test/resolver.rb
@@ -1,0 +1,31 @@
+class ResolverSpec
+  include Theorem::Hypothesis
+
+  test "resolves git" do
+    resolver = resolve(git: "https://something.git")
+
+    expect(resolver.class).to be(::Barista::GitResolver)
+  end
+
+  test "resolves github" do
+    resolver = resolve(github: "skinnyjames/theorem")
+
+    expect(resolver.class).to be(::Barista::GithubResolver)
+  end
+
+  test "resolves http" do
+    resolver = resolve(http: "http://skinnyjames.net/something.tar.gz")
+
+    expect(resolver.class).to be(::Barista::HTTPResolver)
+  end
+
+  test "resolves local" do
+    resolver = resolve(path: "../some/dep")
+
+    expect(resolver.class).to be(::Barista::LocalResolver)
+  end
+
+  def resolve(**args)
+    ::Barista::Resolver.locate(**args)
+  end
+end