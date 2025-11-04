7e0d3d3cdec143aa415ed262d9f66609edbc936b
Zero Stars <zero@skinnyjames.net>
2025-11-04 12:21:10 -0500

    fix(os): adds os resolution to the gem instead of the cli

--- tools/barista/barista.c
+++ tools/barista/barista.c
@@ -10,20 +10,12 @@
 #define OPTPARSE_IMPLEMENTATION
 #define OPTPARSE_API static
 #include "optparse.h"
-#include "platform.h"
 
-mrb_value mrb_barista_os(mrb_state* mrb, mrb_value self)
-{
-  return mrb_str_new_cstr(mrb, platform_os_string(platform_detect_os()));
-}
 
 int main(int argc, char *argv[])
 {
   mrb_state* mrb = mrb_open();
   mrb_mruby_bin_barista_gem_init(mrb);
-
-  struct RClass* barista = mrb_module_get(mrb, "Barista");
-  mrb_define_class_method(mrb, barista, "os", mrb_barista_os, MRB_ARGS_NONE());
 
   struct optparse options;
   optparse_init(&options, argv);
